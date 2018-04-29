4c2227f903543e8bcda827d2900e4b363f3460a2
Alejandro Lafourcade Despaigne <alafourcadedespaigne@gmail.com>
2018-04-29 04:57:57 -0400

    Servicio de Firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,15 +6,33 @@ export class InformacionService {
 
   info: any = {};
   cargada = false;
+  cargada_nosotros = false;
+  equipo: any[] = [];
 
   constructor(public http: HttpClient) {
 
+      this.carga_info();
+      this.cargar_nosotros();
+
+  }
+
+  public carga_info() {
     this.http.get('assets/data/info.pagina.json')
       .subscribe(data => {
-        console.log(data);
         this.cargada = true;
         this.info = data;
       });
+  }
+
+  public cargar_nosotros() {
+
+    this.http.get('https://portafolio-web-378d7.firebaseio.com/equipo.json')
+      .subscribe(data => {
+        console.log(data);
+        this.cargada_nosotros = true;
+        this.equipo = data;
+      });
+
 
   }
 
