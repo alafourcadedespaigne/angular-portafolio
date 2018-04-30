43d4d7c0878a6cdba9de508d33b0d736a2cbc359
Alejandro Lafourcade Despaigne <alafourcadedespaigne@gmail.com>
2018-04-30 00:53:57 -0400

    Primera version

--- src/app/components/buscar/buscar.component.ts
+++ src/app/components/buscar/buscar.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ProductosService} from '../../services/productos.service';
 
 @Component({
   selector: 'app-buscar',
@@ -7,8 +9,17 @@ import {Component} from '@angular/core';
 })
 export class BuscarComponent {
 
-  constructor() {
-  }
+  termino: string = undefined;
 
+  constructor(private route: ActivatedRoute, private _ps: ProductosService) {
+
+    route.params.subscribe(parametros => {
+      this.termino = parametros['termino'];
+
+
+      _ps.buscar_producto(this.termino);
+
+    });
+  }
 
 }
--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,6 +5,7 @@ import {HttpClient} from '@angular/common/http';
 export class ProductosService {
 
   productos: any[] = [];
+  productos_filtrados: any[] = [];
   cargando_productos = true;
 
   constructor(private http: HttpClient) {
@@ -13,6 +14,35 @@ export class ProductosService {
 
   }
 
+
+  public buscar_producto(termino: string) {
+
+
+
+
+    if (this.productos.length === 0) {
+      this.cargar_productos().then(() => {
+
+        // Termino la carga
+        this.filtrar_productos(termino);
+      });
+    } else {
+      this.filtrar_productos(termino);
+    }
+  }
+
+  private filtrar_productos(termino: string) {
+
+    this.productos_filtrados = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach(prod => {
+      if (prod.categoria.indexOf(termino) >= 0 || prod.titulo.toLowerCase().indexOf(termino) >= 0) {
+        this.productos_filtrados.push(prod);
+      }
+
+    });
+  }
 
   public cargar_producto(cod: string) {
     return this.http.get(`https://portafolio-web-378d7.firebaseio.com/productos/${cod}.json`);
@@ -23,14 +53,18 @@ export class ProductosService {
 
     this.cargando_productos = true;
 
-    if (this.productos.length === 0) {
+    const promesa = new Promise((resolve, reject) => {
+
       this.http.get('https://portafolio-web-378d7.firebaseio.com/productos_idx.json')
-        .subscribe( res => {
+        .subscribe(res => {
           this.cargando_productos = false;
           this.productos = res;
+          resolve();
         });
 
-    }
+    });
+
+    return promesa;
   }
 
 }
